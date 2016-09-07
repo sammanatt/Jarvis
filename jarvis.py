473a3ea8bea87710333df4206bed5f9148501e3b
sammanatt <sammanatt09@gmail.com>
2016-09-06 23:13:14 -0500

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,6 @@
 """
+    Base code for Slackbot sourced from   
+
     SCRIPT REQUIREMENTS
         Python
         slackclient API wrapper
@@ -8,9 +10,14 @@
 """
 import os
 import time
+import requests
 from datetime import datetime,tzinfo,timedelta
 from slackclient import SlackClient
 
+
+##################################################################
+#Current Time
+##################################################################
 #Class handles time conversion from UTC to CST
 class Zone(tzinfo):
     def __init__(self,offset,isdst,name):
@@ -27,6 +34,58 @@ class Zone(tzinfo):
 #Update the first parameter during Daylight Savings
 CST = Zone(-5,False,'CST')
 
+current_date_time = datetime.now(CST).strftime('%m/%d/%Y %H:%M')
+
+##################################################################
+#Current Weather
+##################################################################
+
+#Selma TX city ID = 4727785
+#For different city IDs, visit http://bulk.openweathermap.org/sample/
+weather_r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=4727785&units=imperial&APPID=0cd3eb7242f017f9529514bde44103a5')
+#for full list, print r.json()
+
+weather_main = weather_r.json()['main']
+weather_current = "*Temp:* " + str(weather_main['temp'])[0:2]
+weather_max = "*Max:* " + str(weather_main['temp_max'])[0:2]
+weather_min = "*Min:* " + str(weather_main['temp_min'])[0:2]
+
+
+##################################################################
+# Giphy Search
+##################################################################
+giphy = ""
+def giphy_random(giphy_search_query):
+    # Request
+    # GET http://api.giphy.com/v1/gifs/search
+    #http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=oops
+    #http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+
+    try:
+        response = requests.get(
+            url="http://api.giphy.com/v1/gifs/random",
+            params={
+                "api_key": "dc6zaTOxFJmzC",
+                "limit": "1",
+                "fmt": "json",
+                "tag": str(giphy_search_query),
+            },
+        )
+        #print('Response HTTP Status Code: {status_code}'.format(
+        #    status_code=response.status_code))
+        #print('Response HTTP Response Body: {content}'.format(
+        #    content=response.content))
+        giphy_response_json = response.json()['data']
+        global giphy
+        giphy = giphy_response_json['fixed_height_downsampled_url']
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+##########################################################################################
+# Slackbot stuff I don't understand
+# Base code from https://www.fullstackpython.com/blog/build-first-slack-bot-python.htmlBOT
+# Eventually --- Use message buttons for coffee confirmation https://api.slack.com/docs/message-buttons 
+##########################################################################################
 
 # starterbot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
@@ -34,6 +93,7 @@ BOT_ID = os.environ.get("BOT_ID")
 # constants
 AT_BOT = "<@" + BOT_ID + ">:"
 EXAMPLE_COMMAND = "do"
+MORNING_COMMAND = "good morning"
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -45,11 +105,12 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces." + \
-               "...by the way it is " + datetime.now(CST).strftime('%m/%d/%Y %H:%M:%S %Z') + "!!"
+    giphy_random("error 404")
+    response = "Uh oh, an error occurred!" + "\n" + giphy
     if command.startswith(EXAMPLE_COMMAND):
         response = "Sure...write some more code then I can do that!"
+    if command.startswith(MORNING_COMMAND):
+        response = "Good morning & thank klevin, it is " + current_date_time + "\n" + "Oh, and here's the current weather: " + "\n" + weather_current + "\n" + weather_max + "\n" + weather_min +"\n"+"\n"+":rat:"
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
